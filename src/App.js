4414e5a7f0140affa9c46e26d6a3b1b415dcfa0f
sayali199918 <sayaliprashantbadgujar@gmail.com>
2021-07-29 07:48:42 +0530

    React

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Hello Family....Tho kaise ho app log
+        i hope you guys doing well...!!!
+
         </p>
         <a
           className="App-link"
@@ -15,8 +17,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          Learn React        
+           </a>
       </header>
     </div>
   );
